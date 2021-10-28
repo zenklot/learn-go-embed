bff2744c762fe0c200fb0fca4afe8ab1a4551c88
zenklot <zenklot11@gmail.com>
2021-10-28 20:14:39 +0700

    build go file with embed

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+)
+
+//go:embed version.txt
+var version string
+
+//go:embed logo.png
+var logo []byte
+
+//go:embed files/*.txt
+var path embed.FS
+
+func main() {
+	fmt.Println(version)
+
+	err := ioutil.WriteFile("new_logo.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	dir, _ := path.ReadDir("files")
+	for _, entry := range dir {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			content, _ := path.ReadFile("files/" + entry.Name())
+			fmt.Println("Content:", string(content))
+		}
+	}
+}
